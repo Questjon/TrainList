202216e58bebbf82ea4aeb13460c21262e946ae8
Questjon <questjon@hotmail.com>
2017-08-15 22:40:04 +0100

    Added code to parse data
    Now shows train number and location

--- trains.js
+++ trains.js
@@ -1,1 +1,46 @@
-var trains = https://api.tfl.gov.uk/Line/piccadilly/arrivals;
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var trainJSON = JSON.parse(this.responseText);
+        var sortedArray = parseTrains(trainJSON);
+        outputTrains(sortedArray);
+    }
+};
+
+
+xhr.open("GET", "https://api.tfl.gov.uk/Line/piccadilly/arrivals", true);
+xhr.send();
+
+
+function parseTrains(array) {
+
+
+  function hash(o){
+      return o.vehicleId;
+  }
+  var hashesFound = {};
+  array.forEach(function(o){
+      hashesFound[hash(o)] = o;
+  })
+  var results = Object.keys(hashesFound).map(function(k){
+      return hashesFound[k];
+  })
+  results.sort(function(a, b) {
+      return parseFloat(a.vehicleId) - parseFloat(b.vehicleId);
+  });
+
+  return results;
+}
+
+function outputTrains(array)
+{
+  console.log(array);
+  for (var item in array) {
+      var newElement = document.createElement('div');
+      newElement.id = array[item]; newElement.className = "train";
+      newElement.innerHTML = array[item].vehicleId+" : "+array[item].currentLocation;
+      document.body.appendChild(newElement);
+  }
+
+}
